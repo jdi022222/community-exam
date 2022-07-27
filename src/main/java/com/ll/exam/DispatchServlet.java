63e38e08b87240f64b40497ad121666f4274059d
jdi022222 <jdi022222@gmail.com>
2022-07-27 16:53:00 +0900

    라우팅 기능 구현

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import com.ll.exam.article.ArticleController;
+import com.ll.exam.member.MemberController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,10 +11,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/usr/*")
-//usr로 시작하는 url을 모은다
+// usr로 시작하는 url을 모은다
 public class DispatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().append("Work?");
+        Rq rq = new Rq(req,resp);
+
+        MemberController memberController = new MemberController();
+        ArticleController articleController = new ArticleController();
+
+        // url을 변수에 담아서 switch로 구별
+        // getRequestURI는
+        // http://localhost:8081/usr/article/list/free?page=1 에서
+        // /usr/article/list/free 부분만 가져온다.
+        String url = req.getRequestURI();
+
+        switch (url) {
+            case "/usr/article/list/free" :
+                articleController.showList(rq);
+                break;
+            case "/usr/member/login" :
+                memberController.showLogin(rq);
+                break;
+        }
     }
 }
